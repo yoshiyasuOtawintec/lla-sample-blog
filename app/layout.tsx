99c996decc0cef7d40229e9c62825f6681754ced
yoshiyasuOtawintec <yoshiyasu.ota@win-tec.jp>
2026-06-10 16:10:05 +0900

    20260610_official

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import './globals.css';
 import styles from './layout.module.css';
 
 export const metadata = {
-  metadataBase: new URL(process.env.BASE_URL || 'https://lla-sample-blog.vercel.app'),
+  metadataBase: new URL(process.env.BASE_URL || 'https://lla-official-blog.vercel.app'),
   title: {
     // 記事ページなどのタイトルテンプレート
     template: '%s | LLA株式会社公式ブログ',
